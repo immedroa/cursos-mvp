f6ff06a645b4cc86b7f75bf2eb61869eb8b245ab
immedroa <medroavargas@gmail.com>
2026-05-06 04:37:58 -0500

    seo: implementation of redirects for new course slugs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/plataforma/curso-ventas-digitales',
+        destination: '/plataforma/curso-introduccion-blockchain',
+        permanent: true,
+      },
+      {
+        source: '/plataforma/curso-productividad',
+        destination: '/plataforma/curso-blockchain-cripto-peru',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
